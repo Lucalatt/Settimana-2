b2d039822a2d1519c0eb27e3923aca87362e7590
Lucalatt <113041623+Lucalatt@users.noreply.github.com>
2023-01-20 19:01:12 +0300

    Esame Settimanale 20.01
    Aggiunto esame 20.01

--- Esercizi - 19.01.23/Esercizi_19_01_23/src/esercizio19_01_23/Main.java
+++ Esercizi - 19.01.23/Esercizi_19_01_23/src/esercizio19_01_23/Main.java
@@ -31,49 +31,78 @@ public class Main {
 			});
 			
 			ArrayList<Prodotto> ordine1 = new ArrayList<Prodotto>(Arrays.asList(p6, p8, p9));
-			
 			ArrayList<Prodotto> ordine2 = new ArrayList<Prodotto>(Arrays.asList(p3, p5, p8, p9)); 
-			
 			ArrayList<Prodotto> ordine3 = new ArrayList<Prodotto>(Arrays.asList(p1, p8, p9)); 
+			ArrayList<Prodotto> ordine4 = new ArrayList<Prodotto>(Arrays.asList(p2, p7, p8));
 			
 			Cliente c1 = new Cliente(1, "Luca", 1);
 			Cliente c2 = new Cliente(2, "Marco", 2);
 			Cliente c3 = new Cliente(3, "Giovanni", 2);
+			Cliente c4 = new Cliente(4, "Marco", 1);
+			Cliente c5 = new Cliente(4, "Nicola", 2);
 			
-			Ordine o1 = new Ordine (1, "in consegna",LocalDate.of(2023, 1, 23), LocalDate.now(), ordine1, c1);
-			Ordine o2 = new Ordine (2, "in consegna",LocalDate.of(2023, 1, 18), LocalDate.now(), ordine2, c2);
-			Ordine o3 = new Ordine (3, "in consegna",LocalDate.of(2023, 1, 20), LocalDate.now(), ordine3, c3);
+			Ordine o1 = new Ordine(1, "In consegna", LocalDate.now(), LocalDate.of(2023, 01, 23), ordine1, c1);
+			Ordine o2 = new Ordine(2, "In consegna", LocalDate.of(2021, 03, 01), LocalDate.of(2023, 01, 21), ordine2, c2);
+			Ordine o3 = new Ordine(3, "In consegna", LocalDate.now(), LocalDate.of(2023, 01, 22), ordine3, c3);
+			Ordine o4 = new Ordine(3, "In consegna", LocalDate.of(2021, 02, 12), LocalDate.of(2023, 01, 18), ordine3, c4);
+			Ordine o5 = new Ordine(3, "In consegna", LocalDate.of(2021, 01, 12), LocalDate.of(2023, 10, 03), ordine3, c5);
 			
-			ArrayList<Ordine> listaOrdini = new ArrayList<Ordine>(Arrays.asList(o1, o2, o3));
+			//---------------------------------------------------------------------------------
+			
+			ArrayList<Ordine> listaOrdini = new ArrayList<Ordine>(Arrays.asList(o1, o2, o3, o4, o5));
 			
 			ArrayList<Ordine> listaFiltrata = new ArrayList<>();
-	          listaOrdini.stream()
-	                      .filter((ordine) -> ordine.getProdotti().stream()
-	                                      .filter((categoriaProdotto) -> categoriaProdotto.getCategoria() == "Baby")
-	                                      .count() > 0)
-	                      .forEach((ordine) -> listaFiltrata.add(ordine));
+	        listaOrdini.stream()
+	                    .filter((ordine) -> ordine.getProdotti().stream()
+	                                    .filter((categoriaProdotto) -> categoriaProdotto.getCategoria() == "Baby")  
+	                                    .count() > 0)
+	                    .forEach((ordine) -> listaFiltrata.add(ordine));
 
-	            listaFiltrata.forEach(ordine -> {
-	                System.out.println(ordine);
-	              });
-	            
-	            
+			System.out.println( "---------------------------------------" );
+			System.out.println( "Ordini con libri di catergoria BABY:" );
+	        listaFiltrata.forEach(ordine -> {
+	            System.out.println(ordine);
+	          });
+	        
+	        // -------------------------------------------------------------------------------------------
+
+	        System.out.println( "---------------------------------------" );
+			System.out.println( "Ordini con libri di catergoria BOYS con il 10% di sconto:" );
+	        prodotti.stream()
+	        	.filter((prodotto) -> prodotto.getCategoria() == "Boys")
+	        	.peek(prezzo -> prezzo.setPrezzo(prezzo.getPrezzo() - (prezzo.getPrezzo() * 10) / 100))
+	        	.forEach((prodotto) -> System.out.println(prodotto));
+	        
+	        //----------------------------------------------------------------------
+	        
+	        System.out.println( "---------------------------------------" );
+			System.out.println( "Ordini di clienti con livello 2, ordinati tra il 01-Feb-2021 e l'01-Apr-2021:" );
+	        listaOrdini.stream()
+	        	.filter( (o) -> o.getDataOrdine().isAfter(LocalDate.of(2021, 02, 01))
+	        	&& o.getDataOrdine().isBefore(LocalDate.of(2021, 04, 01))
+	        	&& o.getCliente().getLivello() == 2)
+	        	.forEach((o) -> System.out.println(o));
+	      
+		}
+
 	}
-	
-}
 
 	class Prodotto {
-
+		
 		long id;
 		String nome;
 		String categoria;
 		double prezzo;
-
+		
 		public Prodotto(long id, String nome, String categoria, double prezzo) {
 			this.id = id;
 			this.nome = nome;
 			this.categoria = categoria;
 			this.prezzo = prezzo;
+		}
+
+		public Object setPrezzo(double prezzo) {
+			return this.prezzo = prezzo;
 		}
 		
 		public String getCategoria() {
@@ -84,24 +113,24 @@ public class Main {
 			return this.prezzo;
 		}
 		
-		@Override 
+		@Override
 		public String toString() {
-			return "Nome libro: " + this.nome + " Prezzo: " + this.prezzo + " €";
+			return "Nome libro: " + this.nome + ", Categoria: " + this.categoria + ", Prezzo: €" + this.prezzo;
 		}
+		
 	}
 
 	class Ordine {
-
+		
 		long id;
 		String stato;
 		LocalDate dataOrdine;
 		LocalDate dataConsegna;
 		List<Prodotto> prodotti;
 		Cliente cliente;
-
+		
 		public Ordine(long id, String stato, LocalDate dataOrdine, LocalDate dataConsegna, List<Prodotto> prodotti,
 				Cliente cliente) {
-
 			this.id = id;
 			this.stato = stato;
 			this.dataOrdine = dataOrdine;
@@ -109,34 +138,43 @@ public class Main {
 			this.prodotti = prodotti;
 			this.cliente = cliente;
 		}
-		
-		public List<Prodotto> getProdotti() {
-			return this.prodotti
-;			
+
+		public Cliente getCliente() {
+			return this.cliente;
+		}
+
+		public LocalDate getDataOrdine() {
+			return this.dataOrdine;
+		}
+
+		@Override
+		public String toString() {
+			return "Stato dell'ordine: " + this.stato + ", Data di acquisto: " + this.dataOrdine + ", Data di cosegna: " + this.dataConsegna + ", Cliente: " + this.cliente + ", Prodotti: " + this.prodotti;
 		}
 		
-		@Override 
-		public String toString() {
-			return "ID - " + this.id + " stato ordine: " + this.stato + " data ordine: " + this.dataOrdine + " data consegna: " + this.dataConsegna +" prodotti: " + this.prodotti + " Cliente: " + this.cliente;
+		public List<Prodotto> getProdotti() {
+			return this.prodotti;
 		}
 	}
 
 	class Cliente {
-
+		
 		long id;
 		String nome;
 		int livello;
-
+		
 		public Cliente(long id, String nome, int livello) {
-
 			this.id = id;
 			this.nome = nome;
 			this.livello = livello;
 		}
 		
-		@Override 
-		public String toString() {
-			return this.nome + " Livello: " + this.livello;
+		public int getLivello() {
+			return this.livello;
 		}
-
+		
+		@Override
+		public String toString() {
+			return this.nome + ", Livello: " + this.livello;
+		}
 	}
